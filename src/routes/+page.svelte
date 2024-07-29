38f7280eea45420fc1d12ea5370363d006ad2325
Bharat Naik <bharat.anaik2003@gmail.com>
2024-07-29 14:38:15 +0530

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="card mt-3 shadow">
+        <div class="card-body">
+            <h1>Bharat Ashok Naik</h1>
+            <p>college: <b>Cambridge Institute of Technology, Bengaluru</b></p>
+            <p>email: <a href="mailto:bharat.anaik2003@gmail.com">bharat.anaik2003@gmail.com</a></p>
+            <p>phone: <a href="tel:+9980776034">+919980776034</a></p>
+        </div>
+    </div>
+</div>
